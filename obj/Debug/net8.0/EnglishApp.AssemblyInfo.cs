48ec2dcb97e2a3e37a60974e8d74c66cd1cd2340
shaked <shakedbenezra100@gmail.com>
2026-06-26 17:34:01 +0300

    table and game page

--- obj/Debug/net8.0/EnglishApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/EnglishApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnglishApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bbdb1bc5e476a980ecb794745a61a5bca755d2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+298a3cde60b1593ea1fc27794fa30455a2bde0dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnglishApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnglishApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
